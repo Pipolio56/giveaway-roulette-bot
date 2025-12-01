a4290721c800a6e429c93947c97fb5a7b410a952
Pipolio56 <pipolioclash@gmail.com>
2025-12-01 23:46:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import discord
 from discord.ext import commands
 from PIL import Image, ImageDraw, ImageFont
 
-TOKEN = os.environ["DISCORD_TOKEN"]
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
